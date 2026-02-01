37467aa66933874ff937a0b2c75072ab5976d676
Rishabh Upadhyay <97178333+Rishabhupadhya@users.noreply.github.com>
2026-02-02 00:35:48 +0530

    Delete frontend directory

--- frontend/src/middleware/error.middleware.ts
+++
@@ -1,6 +0,0 @@
-export const handleError = (error: any): string => {
-  if (error.response?.data?.message) {
-    return error.response.data.message;
-  }
-  return "Something went wrong";
-};
--- frontend/src/services/activity.service.ts
+++
@@ -1,6 +0,0 @@
-import { api } from "./api";
-
-export const getTeamActivity = async (teamId: string, limit: number = 50) => {
-  const res = await api.get(`/team/${teamId}/activity?limit=${limit}`);
-  return res.data;
-};
