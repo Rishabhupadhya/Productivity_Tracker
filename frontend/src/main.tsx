37f7a95ea5d89997b4135f67f92d66aa2dcdd68c
Rishabh Upadhyay <rishabhupadhyay@Rishabhs-MacBook-Air.local>
2026-02-03 00:54:52 +0530

    Add comprehensive mobile responsive design for all devices

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App";
 import { UserProvider } from "./contexts/UserContext";
 import { TeamProvider } from "./contexts/TeamContext";
 import "./index.css";
+import "./responsive.css"; // Mobile responsive styles
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
